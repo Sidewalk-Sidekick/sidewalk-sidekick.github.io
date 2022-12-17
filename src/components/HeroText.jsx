376225f95ad081d090fda7356d7fc8f805f673be
nathanspeich <nathanspeich@icloud.com>
2022-12-16 23:21:42 -0500

    reduce font size for 700px media query breakpoint

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -4,7 +4,7 @@ const HeroText = () => {
   return (
     <>
       <p className='heroText'>
-        Get the most accessible routes in real-time from a dedicated community.
+        Discover accessible routes in real-time from a dedicated community.
       </p>
       <p className='heroStatus'>Alpha testing coming 2023</p>
     </>
