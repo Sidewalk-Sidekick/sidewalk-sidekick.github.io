c418f45cbeac4f841fc997334e3e73eae08534bb
nathanspeich <nathanspeich@icloud.com>
2023-02-07 18:31:08 -0500

    remove sign up component import

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ import HeroHeader from './HeroHeader';
 import HeroText from './HeroText';
 import HeroNav from './HeroNav';
 import '../ui/Hero.css';
-import SignUp from './SignUp';
 
 const Hero = () => {
   return (
@@ -10,7 +9,6 @@ const Hero = () => {
       <div className='hero container'>
         <HeroHeader title='Sidewalk Sidekick' />
         <HeroText />
-        {/* <SignUp title='User Testing Sign Up' /> */}
         <HeroNav />
       </div>
     </section>
