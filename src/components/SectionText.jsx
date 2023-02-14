7c3d427df5164560522c44b4fe6827ad42ddf026
nathanspeich <nathanspeich@icloud.com>
2023-02-14 13:31:28 -0500

    create section text component

---
+++ src/components/SectionText.jsx
@@ -0,0 +1,11 @@
+import '../ui/SectionText.css';
+
+const SectionText = () => {
+  return (
+    <p className='section-text'>
+      <>section text goes here</>
+    </p>
+  );
+};
+
+export default SectionText;
