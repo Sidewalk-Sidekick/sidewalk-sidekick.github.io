16b518409df43bd579884557ec4e844f828c1358
nathanspeich <nathanspeich@icloud.com>
2023-02-14 13:01:33 -0500

    create main nav menu component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from '../components/Hero';
+import MainNav from '../components/MainNav';
 
 const Home = () => {
   return (
     <div className='pageContainer'>
       <Hero />
+      <MainNav />
     </div>
   );
 };
