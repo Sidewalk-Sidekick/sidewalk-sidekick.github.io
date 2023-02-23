b02ec4dce2e035250ef20d9dab9637c3d54a9bc3
noelcastillon1 <noelcastillon1@gmail.com>
2023-02-23 15:35:09 -0500

    created teamhistory component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from '../components/Hero';
+import TeamHistory from '../components/TeamHistory';
 
 const Home = () => {
   return (
     <div className='page-container'>
       <Hero />
+      <TeamHistory />
     </div>
   );
 };
