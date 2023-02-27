f5a88af45221b5b0158a9acd0c5f99c0d3ada0c9
noelcastillon1 <noelcastillon1@gmail.com>
2023-02-26 19:56:27 -0500

    add content to TeamHistory component

--- src/components/TeamHistory.jsx
+++ src/components/TeamHistory.jsx
@@ -10,21 +10,55 @@ import '../ui/TeamHistory.css';
 const TeamHistory = () => {
     return (
         <Section>
-            <SectionHeader>
-                <SectionHeading title="Team History" />
-            </SectionHeader>
-            <SectionContent>
-                <SectionTextBox>
-                    <SectionText>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aut atque aliquam non saepe exercitationem molestias sint, voluptatem et. Eos obcaecati eius ratione repellendus esse ad, distinctio ipsam velit nihil?
-                        </p>
-                    </SectionText>
-                </SectionTextBox>
-                <SectionMedia>
-                    <img src="./groupPhoto.jpg" alt="Photo of the team from GiveBackHack Columbus 2022" />
-                </SectionMedia>
-            </SectionContent>
+            <div className="teamHistory">
+                <SectionHeader>
+                    <SectionHeading title="Team History" />
+                </SectionHeader>
+                <div>
+                    <SectionContent>
+                        <SectionMedia>
+                            <img src="./groupPhotoTeam.jpg" alt="Photo of the team from GiveBackHack Columbus 2022" />
+                        </SectionMedia>
+                    </SectionContent>
+                    <SectionContent>
+                        <SectionTextBox>
+                            <SectionText>
+                                <p>
+                                    Conceived between friends passionate about the viability of walkable communities, Sidewalk Sidekick was the winning pitch of GiveBackHack Columbus 2022.
+                                </p>
+                            </SectionText>
+                        </SectionTextBox>
+                    </SectionContent>
+                    <SectionContent>
+                        <SectionTextBox>
+                            <SectionText>
+                                <p>
+                                    Composed of a healthy mix of developers, designers, and product growth researchers, we’re a small team committed to making alternative forms of transportation safe, accessible, and viable.
+                                </p>
+                            </SectionText>
+                        </SectionTextBox>
+                    </SectionContent>
+                    <SectionContent>
+                        <SectionMedia>
+                            <img src="./awardsPhoto.jpeg" alt="Photo of the team from GiveBackHack Columbus 2022" />
+                        </SectionMedia>
+                    </SectionContent>
+                    <SectionContent>
+                        <SectionMedia>
+                            <img src="./groupPhotoGBH.jpg" alt="Photo of the team from GiveBackHack Columbus 2022" />
+                        </SectionMedia>
+                    </SectionContent>
+                    <SectionContent>
+                        <SectionTextBox>
+                            <SectionText>
+                            <a href='http://localhost:3000/' className='btn btn-dark'>
+                                Click here to view our slidedeck from GiveBackHack Columbus 2022
+                            </a>
+                            </SectionText>
+                        </SectionTextBox>
+                    </SectionContent>
+                </div>
+            </div>
         </Section>
     );
   };
