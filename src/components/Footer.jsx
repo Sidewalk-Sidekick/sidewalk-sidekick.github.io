308981d1ae54094170327ed57b75ac21df21adaf
noelcastillon1 <noelcastillon1@gmail.com>
2023-03-01 17:10:50 -0500

    edited footer tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import '../ui/Footer.css';
 
 const Footer = (props, { className }) => {
-  return <nav className={`footer ${props.className}`}>{props.children}</nav>;
+  return <footer className={`footer ${props.className}`}>{props.children}</footer>;
 };
 
 export default Footer;
