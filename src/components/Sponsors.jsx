6b93f25bd74b624e433bac9ce93323e9c233c13f
nathanspeich <nathanspeich@icloud.com>
2023-03-02 21:45:46 -0500

    replace placeholder partnership images

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -2,27 +2,38 @@ import Section from './Section';
 import SectionHeader from './SectionHeader';
 import SectionHeading from './SectionHeading';
 import SectionContent from './SectionContent';
-import '../ui/Partnerships.css';
+import '../ui/Sponsors.css';
 import Cards from './Cards';
 import Card from './Card';
 import CardContent from './CardContent';
 import CardMedia from './CardMedia';
 
-const Partnerships = () => {
+const Sponsors = () => {
   return (
-    <Section className='partners'>
+    <Section className='sponsors'>
       <SectionHeader>
-        <SectionHeading title='Partnerships' />
+        <SectionHeading title='Sponsors' />
       </SectionHeader>
       <SectionContent className='container'>
         <Cards className='grid grid-4-cols'>
           <Card>
             <CardContent>
               <CardMedia>
-                <img
-                  src='https://dummyimage.com//250x250/ddddddffffff'
-                  alt='logo'
-                />
+                <img src='give-back-hack-logo.png' alt='logo' />
+              </CardMedia>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent>
+              <CardMedia>
+                <img src='code-and-coffee-logo.jpeg' alt='logo' />
+              </CardMedia>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent>
+              <CardMedia>
+                <img src='united-way-logo.png' alt='logo' />
               </CardMedia>
             </CardContent>
           </Card>
@@ -30,27 +41,7 @@ const Partnerships = () => {
             <CardContent>
               <CardMedia>
                 <img
-                  src='https://dummyimage.com//250x250/ddddddffffff'
-                  alt='logo'
-                />
-              </CardMedia>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent>
-              <CardMedia>
-                <img
-                  src='https://dummyimage.com//250x250/ddddddffffff'
-                  alt='logo'
-                />
-              </CardMedia>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent>
-              <CardMedia>
-                <img
-                  src='https://dummyimage.com//250x250/ddddddffffff'
+                  src='https://dummyimage.com/600x600/cccccc/ffffff.png&text=Venue+Sponsor'
                   alt='logo'
                 />
               </CardMedia>
@@ -58,11 +49,11 @@ const Partnerships = () => {
           </Card>
         </Cards>
         <a href='http://localhost:3000/' className='btn btn-dark'>
-          Become a Partner
+          Become a Sponsor
         </a>
       </SectionContent>
     </Section>
   );
 };
 
-export default Partnerships;
+export default Sponsors;
