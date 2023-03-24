f3321845c8a56cb645d6abe289bf7116b4e588b6
noelcastillon1 <noelcastillon1@gmail.com>
2023-03-24 16:08:58 -0700

    edited footer imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import Hero from '../components/Hero';
-import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div className='page-container'>
       <Hero />
-      <Footer />
-    </div>
+=    </div>
   );
 };
 
