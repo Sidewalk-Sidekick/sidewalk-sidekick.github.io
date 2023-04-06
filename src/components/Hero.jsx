77669d62b806da0bec48ac01075f3ca1d787bf4c
nathanspeich <nathanspeich@icloud.com>
2023-04-06 13:49:51 -0400

    style hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
+import MainHeader from '../components/MainHeader';
 import HeroHeader from './HeroHeader';
 import HeroText from './HeroText';
-import HeroNav from './HeroNav';
 import '../ui/Hero.css';
+import HeroNav from '../components/HeroNav';
 
 const Hero = () => {
   return (
     <section className='sectionHero' id='hero'>
+      {/* <MainHeader /> */}
       <div className='hero container'>
         <HeroHeader title='Sidewalk Sidekick' />
         <HeroText />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import Hero from '../components/Hero';
+import HowItWorks from '../components/HowItWorks';
+import Why from '../components/Why';
+import OurStory from '../components/OurStory';
+import MeetTheTeam from '../components/MeetTheTeam';
+import Sponsors from '../components/Sponsors';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div className='page-container'>
       <Hero />
+      {/* <HowItWorks />
+      <Why />
+      <OurStory />
+      <MeetTheTeam />
+      <Sponsors />
+      <Footer /> */}
     </div>
   );
 };
