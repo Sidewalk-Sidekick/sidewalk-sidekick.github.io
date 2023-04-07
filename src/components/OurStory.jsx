f91ac976b75d865c5af4b8d5b3e86619ac85a4b1
nathanspeich <nathanspeich@icloud.com>
2023-04-07 18:18:13 -0400

    style our story section component

--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -6,6 +6,11 @@ import SectionText from './SectionText';
 import SectionContent from './SectionContent';
 import SectionMedia from './SectionMedia';
 import '../ui/OurStory.css';
+import Cards from '../components/Cards';
+import Card from '../components/Card';
+import CardMedia from '../components/CardMedia';
+import CardTextBox from '../components/CardTextBox';
+import CardText from '../components/CardText';
 
 const OurStory = () => {
   return (
@@ -14,59 +19,56 @@ const OurStory = () => {
         <SectionHeading title='Our Story' />
       </SectionHeader>
       <SectionContent className='container'>
-        <div className='grid grid-2-cols'>
-          <SectionMedia>
-            <img
-              src='./awardsPhoto.jpeg'
-              alt='Photo of the team from GiveBackHack Columbus 2022'
-            />
-          </SectionMedia>
-          <SectionTextBox>
-            <SectionText>
-              <p>
+        <Cards>
+          <Card>
+            <CardMedia>
+              <img
+                src='./awardsPhoto.jpeg'
+                alt='Photo of the team from GiveBackHack Columbus 2022'
+              />
+            </CardMedia>
+            <CardTextBox>
+              <CardText>
                 Conceived between friends passionate about the viability of
                 walkable communities, Sidewalk Sidekick was the winning pitch of
                 GiveBackHack Columbus 2022.
-              </p>
-            </SectionText>
-          </SectionTextBox>
-        </div>
-        <div className='grid grid-2-cols'>
-          <SectionTextBox>
-            <SectionText>
-              <p>
+              </CardText>
+            </CardTextBox>
+          </Card>
+          <Card>
+            <CardTextBox>
+              <CardText>
                 Composed of a healthy mix of developers, designers, and product
                 growth researchers, we’re a small team committed to making
-                alternative forms of transportation safe, accessible, and
-                viable.
-              </p>
-            </SectionText>
-          </SectionTextBox>
-          <SectionMedia>
-            <img
-              src='./groupPhotoTeam.jpg'
-              alt='Photo of the team from GiveBackHack Columbus 2022'
-            />
-          </SectionMedia>
-        </div>
-        <div className='grid grid-2-cols'>
-          <SectionMedia>
-            <img
-              src='./groupPhotoGBH.jpg'
-              alt='Photo of the team from GiveBackHack Columbus 2022'
-            />
-          </SectionMedia>
-          <SectionTextBox>
-            <SectionText>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                saepe dolore eligendi dignissimos assumenda aliquam veritatis
-                repellendus! Aspernatur odit sit ab quis eum aperiam, iure
-                ratione ipsum nemo expedita at.
-              </p>
-            </SectionText>
-          </SectionTextBox>
-        </div>
+                alternative forms of travel safe, accessible, and viable.
+              </CardText>
+            </CardTextBox>
+            <CardMedia>
+              <img
+                src='./groupPhotoTeam.jpg'
+                alt='Photo of the team from GiveBackHack Columbus 2022'
+              />
+            </CardMedia>
+          </Card>
+          <Card>
+            <CardMedia>
+              <img
+                src='./groupPhotoGBH.jpg'
+                alt='Photo of the team from GiveBackHack Columbus 2022'
+              />
+            </CardMedia>
+            <CardTextBox>
+              <CardText>
+                Join us April 8, 2023 at 12:00 PM for our first user test event,
+                hosted at COhatch The Gateway. Each applicant (limit 50) will
+                receive a $20 Amazon gift card and pizza!
+              </CardText>
+              <a href='#' className='btn btn-dark'>
+                Sign up to be a tester
+              </a>
+            </CardTextBox>
+          </Card>
+        </Cards>
       </SectionContent>
     </Section>
   );
